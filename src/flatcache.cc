e4419bd19b6b5e7a0b857714185752bf7bee8d9d
Dan Mick <dan.mick@inktank.com>
2012-12-13 14:05:38 -0800

    wip

--- src/flatcache.cc
+++ src/flatcache.cc
@@ -9,6 +9,9 @@
 
 using namespace std;
 
+// wrap a system/library call with return-code checking (if you don't get
+// 'expect', whine and exit
+
 #define WRAP(call, expect)				\
 	{					\
 	int r;					\
@@ -20,6 +23,12 @@ using namespace std;
 
 void read_cb(librbd::completion_t cb, void *arg);
 atomic_t inflight;
+
+struct comparg {
+	uint64_t offset;
+	ceph::bufferlist *blp;
+	librbd::RBD::AioCompletion *cp;
+};
 
 int
 main()
@@ -42,10 +51,10 @@ main()
 	int order = 22;
   	WRAP(R.create2(ioctx, "image", SIZE, RBD_FEATURE_LAYERING, &order), 0);
 
-	librbd::Image image;
-	WRAP(R.open(ioctx, image, "image"), 0);
 
 	{
+		librbd::Image image;
+		WRAP(R.open(ioctx, image, "image"), 0);
 		// fill with random data
 		ifstream urandom("/dev/urandom", ifstream::in);
 		char *b = new char [SIZE];
@@ -54,33 +63,40 @@ main()
 		ceph::bufferlist bl;
 		bl.append(b, SIZE);
 		WRAP(image.write(0, SIZE, bl), SIZE);
+		delete [] b;
+		// image goes out of scope, so, destroyed/closed
 	}
 
+	// reopen image for reading
+	librbd::Image image;
+	WRAP(R.open(ioctx, image, "image"), 0);
 	WRAP(image.snap_create("snap"), 0);
 	WRAP(image.snap_protect("snap"), 0);
-	ceph::bufferlist bl(1);
 	for (;;) {
-		cout << "clone" << endl;
 		WRAP(R.clone(ioctx, "image", "snap", ioctx, "clone",
 			     RBD_FEATURE_LAYERING, &order), 0);
 		{
 			librbd::Image clone;
 			WRAP(R.open(ioctx, clone, "clone", 0), 0);
 			// set up some aio_reads from the parent
-			for (int i = 0; i < 10000; i++) {
-				uint64_t ofs = (((uint64_t)rand() << 32)
+			for (int i = 0; i < 2048; i++) {
+				ceph::bufferlist *blp = new ceph::bufferlist(1);
+				comparg *cap = new comparg;
+				librbd::RBD::AioCompletion *cp = new
+				    librbd::RBD::AioCompletion((void *)cap,
+						 	       read_cb);
+				cap->cp = cp;
+				cap->offset = (((uint64_t)rand() << 32)
 						| rand()) % SIZE;
-				librbd::RBD::AioCompletion
-				    c((void *)ofs, read_cb);
-				// yes, all into same bl
+				cap->blp = blp;
 				inflight.inc();
-				image.aio_read(ofs, 1, bl, &c);
+				image.aio_read(cap->offset, 1, *(cap->blp), cp);
 			}
-			cout << "flatten, inflight: " << inflight.read() << endl;
+			uint64_t before = inflight.read();
 			WRAP(clone.flatten(), 0);
-			cout << "flatten done, inflight: " << inflight.read() << endl;
+			cout << "before flatten: " << before;
+			cout << " after: " << inflight.read() << endl;
 		}
-		cout << "remove" << endl;
 		WRAP(R.remove(ioctx, "clone"), 0);
 	}
 }
@@ -88,7 +104,11 @@ main()
 void
 read_cb(librbd::completion_t cb, void *arg)
 {
+	comparg *cap = (comparg *)arg;
 	inflight.dec();
+	delete cap->blp;
+	cap->cp->release();
+	delete cap;
 	// int ofs = (uint64_t)arg;
 	// cout << "read_cb " << ofs << endl;
 }
